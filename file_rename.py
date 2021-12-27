e8d37bde3275ea62d612765a71ad25f2195ffe63
laurent1900 <waya8158@163.com>
2021-12-27 11:27:47 +0800

    Add files via upload
    create

---
+++ file_rename.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+import os
+import re
+
+def file_rename():
+    pwd = os.getcwdu()
+    for i in os.listdir(pwd):
+        if 'xml' in i:
+            a = re.sub(r"\(.*\)","",i)
+            try:
+                os.rename(i,a)
+            except Exception as e:
+                print('duplication file name: '+a)
+                pass
+
+if __name__ == '__main__':
+    file_rename()
