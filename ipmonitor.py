80cac67ee7dcc41d15c4b0ea2ca12587e2bcf4f7
Laurent1900 <waya8158@163.com>
2022-01-18 13:37:38 +0800

    update

--- ipmonitor.py
+++ ipmonitor.py
@@ -6,7 +6,6 @@ from multiprocessing import Pool
 
 #端口扫描
 def port_scan(host):
-	# ip_port_result = []
 	nmap = nmap3.Nmap()
 	result = nmap.scan_top_ports(host,args="--open -n")
 	ports = result[host]['ports']
@@ -17,25 +16,16 @@ def port_scan(host):
 		print(host,ports_result)
 	else:
 		print(host,'0 port up')
-	# print(ip_port_result)
 
 #主机发现，扫描全部外网IP，检查启用状态
 def host_discovery(ip):
-
-	# host_up = []
-	# host_down = []
-
 	nmap = nmap3.NmapHostDiscovery()
 	result = nmap.nmap_no_portscan(ip)
 	host_status = result['runtime']['summary']
 	if '(1 host up)' in host_status:
 		port_scan(ip)
-		# host_up.append(ip)
 	elif '(0 hosts up)' in host_status:
-		# host_down.append(ip)
 		print(ip+" INACTIVE")
-
-	# return host_up,host_down
 
 #读取IP地址信息
 def file_read(f):
@@ -55,11 +45,4 @@ def run():
 	p.join()
 
 if __name__ == '__main__':
-	# results = []
-	# job_list = file_read('ip2.json')
-	# p = Pool(5)
-	# for i in job_list:
-	# 	p.apply_async(host_discovery,(i,))
-	# p.close()
-	# p.join()
 	run()
