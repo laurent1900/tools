7cad2ca4a9336a2d1a435315e8926d8c199a1749
Laurent1900 <waya8158@163.com>
2022-09-09 16:27:17 +0800

    update
    update

--- ipmonitor.py
+++ ipmonitor.py
@@ -1,48 +1,107 @@
 #coding:utf-8
 import nmap3
 import json
-from multiprocessing import Pool
+import requests
+import re
+import time
+import dingtalkchatbot.chatbot as cb
+from multiprocessing import Pool,Queue
 
 
 #端口扫描
 def port_scan(host):
 	nmap = nmap3.Nmap()
-	result = nmap.scan_top_ports(host,args="--open -n")
-	ports = result[host]['ports']
-	ports_result = []
-	if len(ports) != 0:
-		for j in ports:
-				ports_result.append(j['portid'])
-		print(host,ports_result)
-	else:
-		print(host,'0 port up')
+	result = nmap.scan_top_ports(host,args="--open -sV -n -T5 -Pn -p-")
+	try:
+		ports = result[host]['ports']
+		name = []
+		portid = []
+		product = []
+		service_name = []
+		service = []
+		if len(ports) != 0:
+			for j in ports:
+				portid.append(j['portid'])
+				name.append(j['service']['name'])
+				try:
+					product.append(j['service']['product'])
+				except Exception as e:
+					product.append('unknown service')
+			for i in zip(name,product):
+				service_name.append(': '.join(i))
+			for j in zip(portid,service_name):
+				service.append(' '.join(j))
+			return [host,service]
+	except Exception as e:
+		return [host,'0 port up']
+		pass
 
-#主机发现，扫描全部外网IP，检查启用状态
-def host_discovery(ip):
-	nmap = nmap3.NmapHostDiscovery()
-	result = nmap.nmap_no_portscan(ip)
-	host_status = result['runtime']['summary']
-	if '(1 host up)' in host_status:
-		port_scan(ip)
-	elif '(0 hosts up)' in host_status:
-		print(ip+" INACTIVE")
-
-#读取IP地址信息
+#本地读取IP地址信息
 def file_read(f):
 	f = open(f,'r')
-	text = f.readline()
+	text = []
+	for i in f:
+		text.append(i.strip())
 	f.close()
-	ips = json.loads(text)
-	ips = ips.keys()
+	return text
+
+#本地保存扫描结果
+def mycallback(x):
+	with open('nmap_result.txt','a+') as g:
+		if x[1] != '0 port up':
+			g.write('[+]'+str(x[0])+'\n>>>'+'\n>>>'.join(x[1])+'\n\n')
+		else:
+			pass
+	g.close()
+
+def dingding(msg):
+	webhook = 'XXXXXX'
+	secret = 'XXXXXX'
+	ding = cb.DingtalkChatbot(webhook,secret=secret)
+	ding.send_text(msg=msg, is_at_all=False)
+
+#CMDB获取URL
+def get_ips():
+	f = open('BLJ.csv', mode='a',encoding='utf-8',newline='')
+	url = "XXXXXX"
+	headers = {
+	"Host": "XXXX",
+	"Cookie": "XXXXX"
+	}
+	r = requests.get(url,headers=headers,timeout=30)
+	text = r.text
+	text = text.split('\n')
+	ips = []
+	for i in text:
+		try:
+			ip = i.split(',')[7]
+			if ip != '' and 'IP' not in ip:
+				if '||' in ip:
+					ip = ip.replace('||','')
+					ips.append(ip)
+				else:
+					ips.append(ip)
+		except Exception as e:
+			pass
+	ips = set(ips)
 	return list(ips)
 
-def run():
-	job_list = file_read('ip.json')
-	p = Pool(5)
+def run(x):
+	job_list = x
+	p = Pool(10)
 	for i in job_list:
-		p.apply_async(host_discovery,(i,))
+		p.apply_async(port_scan,(i,),callback=result_get)
 	p.close()
 	p.join()
 
+def result_get(x):
+	result.append(x)
+
 if __name__ == '__main__':
-	run()
+	result = []
+	a = ''
+	run(get_ips())
+	for i in result:
+		if i[1] != '0 port up':
+			a += '[+]'+str(i[0])+'\n>>>'+'\n>>>'.join(i[1])+'\n\n'
+	dingding(a)
