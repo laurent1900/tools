2396a7fee0f67b93a105806c4171421c1072adff
laurent1900 <waya8158@163.com>
2026-02-09 08:21:38 +0800

    update

--- domain2ip.py
+++ domain2ip.py
@@ -5,21 +5,21 @@ import sys
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 resolver = dns.resolver.Resolver()
-resolver.nameservers = ['114.114.114.114']
+resolver.nameservers = ['8.8.8.8']
 
 def main(i):
-	try:
-		domain = i
-		result = resolver.resolve(domain, 'A')
-		for i in result.response.answer:
-			for j in i.items:
-				if j.rdtype == 1:
-					print(domain,j.address)
-	except Exception as e:
-		print(i,e)
-		pass
+    try:
+        domain = i
+        result = resolver.resolve(domain, 'A')
+        for i in result.response.answer:
+            for j in i.items:
+                if j.rdtype == 1:
+                    print(domain,j.address)
+    except Exception as e:
+        print(i,e)
+        pass
 
 if __name__ == '__main__':
-	domains = [""]
-	for i in domains:
-		main(i)
+    domains = ["www.baidu.com"]
+    for i in domains:
+        main(i)
